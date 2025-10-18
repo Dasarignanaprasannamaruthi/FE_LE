d189dfc74563ad42d8545ed6876c7423ab5382c2
Maruthi <maruthinaidua47@gmail.com>
2025-10-18 10:21:05 +0530

    Updated Commit

---
+++ script1.js
@@ -0,0 +1,21 @@
+let a = 18;
+if (a >= 18){
+    console.log("Adult!");
+}
+else{
+    console.log("Minor!");
+}
+
+for (let i = 1; i <10; i++){
+    console.log("iteration Number: "+ i);
+}
+
+function greet(name){
+    console.log("Hello " + name);
+}
+
+greet("Nithish");
+greet("Rakhi");
+
+
+                                                                                                                                                                       
